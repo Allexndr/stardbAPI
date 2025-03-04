b8221f2603a4804c76cbfd4b2a5cf2c0f7327850
Allexndr <your-email@example.com>
2025-03-04 22:38:47 +0500

    step 7 Using props in React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/app';
+import React from "react";
+import ReactDOM from 'react-dom';
+import AppHeader from "../../../../Downloads/todo-54914f644c58880e7aac3888d6ddcf4bcdadd8c2/src/components/app-header";
+import SearchPanel from "../../../../Downloads/todo-54914f644c58880e7aac3888d6ddcf4bcdadd8c2/src/components/search-panel";
+import TodoList from "../../../../Downloads/todo-54914f644c58880e7aac3888d6ddcf4bcdadd8c2/src/components/todo-list";
 
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList/>
+        </div>
+    );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
