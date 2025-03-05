f4a6e1acdf0587dd28f14b1c3ced570e5c3f30b3
Allexndr <your-email@example.com>
2025-03-05 08:46:05 +0500

    Step 9: Introduce Row Component Created a reusable Row component for layout consistency. Improve Item Rendering. Enhanced ItemList with a renderItem prop for customizable item rendering. Updated PeoplePage to use Row for better structure. Applied minor refactoring for readability and maintainability. master 1 parent

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,37 +1,65 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-import Header from '../header'
-import RandomPlanet from '../random-planet'
-import ItemList from '../item-list'
-import PersonDetails from '../person-details'
+import Header from '../header';
+import RandomPlanet from '../random-planet';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+import ErrorIndicator from '../error-indicator';
+import PeoplePage from '../people-page';
+import SwapiService from '../../services/swapi-service';
 
-import './app.css'
+import "./app.css";
 
-export default class App extends Component{
+export default class App extends Component {
+  swapiService = new SwapiService();
 
-    state = {
-        selectedPerson: null
+  state = {
+    selectedPerson: null,
+    hasError: false,
+  };
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
     }
 
-    onPersonSelected = (id) => {
-        this.setState({ selectedPerson: id })
-    }
+    return (
+      <div>
+        <Header />
+        <RandomPlanet />
 
-    render() {
-        return (
-            <div>
-                <Header/>
-                <RandomPlanet/>
+        <PeoplePage />
 
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList onItemSelected={ this.onPersonSelected }/>
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={ this.state.selectedPerson }/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => item.name}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+              renderItem={(item) => item.name}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
