c767335ebee3c78f0bf725067a907defa6c4602b
Allexndr <your-email@example.com>
2025-03-19 08:11:57 +0500

    Step 11: Introduce Higher-Order Components (HOC) and Improve Data Fetching Added a withData HOC to encapsulate data fetching logic and improve code reuse. Refactored ItemList to use withData, removing direct API calls. Enhanced ItemDetails by introducing a Record component for better data management. Implemented Row for a structured layout and wrapped PeoplePage with ErrorBoundary to handle errors gracefully. Updated SwapiService to include additional character attributes for a richer data display.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,38 +1,38 @@
-import React, { Component } from "react";
+import React, {Component} from 'react'
 
-import Header from "../header";
-import RandomPlanet from "../random-planet";
-import ItemList from "../item-list";
-import PersonDetails from "../person-details";
+import Header from '../header'
+import RandomPlanet from '../random-planet'
 
-import "./app.css";
+import './app.css'
 import ErrorIndicator from "../error-indicator";
 import PeoplePage from "../people-page";
 import SwapiService from "../../services/swapi-service";
 
-export default class App extends Component {
-  swapiService = new SwapiService();
+export default class App extends Component{
 
-  state = {
-    selectedPerson: null,
-    hasError: false,
-  };
+    swapiService = new SwapiService()
 
-  componentDidCatch(error, errorInfo) {
-    this.setState({ hasError: true });
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <ErrorIndicator />;
+    state = {
+        selectedPerson: null,
+        hasError: false
     }
 
-    return (
-      <div>
-        <Header />
-        <RandomPlanet />
-        <PeoplePage />
-      </div>
-    );
-  }
+    componentDidCatch(error, errorInfo) {
+        this.setState({ hasError: true })
+    }
+
+    render() {
+
+        if (this.state.hasError) {
+            return <ErrorIndicator />
+        }
+
+        return (
+            <div>
+                <Header/>
+                <RandomPlanet />
+                <PeoplePage />
+            </div>
+        )
+    }
 }
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,43 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-import "./item-list.css";
-import Spinner from "../spinner";
+import './item-list.css'
+import SwapiService from "../../services/swapi-service"
+import withData from "../hoc-helpers/with-data"
 
-export default class ItemList extends Component {
-  state = {
-    itemList: null,
-  };
+const ItemList = (props) =>  {
 
-  componentDidMount() {
-    const { getData } = this.props;
+    const { data, onItemSelected, children: renderLabel } = props
 
-    getData().then((itemList) => {
-      this.setState({ itemList });
-    });
-  }
+    const items = data.map((item) => {
+        const { id } = item
+        const label = renderLabel(item)
 
-  renderItems(arr) {
-    return arr.map((item) => {
-      const { id } = item;
-      const label = this.props.children(item)
-      return (
-        <li
-          className="list-group-item"
-          key={id}
-          onClick={() => this.props.onItemSelected(id)}>
-          {label}
-        </li>
-      );
-    });
-  }
+        return (
+            <li className="list-group-item"
+                key={ id }
+                onClick={ () => onItemSelected(id) }>
+                { label }
+            </li>
+        )
+    })
 
-  render() {
-    const { itemList } = this.state;
-
-    if (!itemList) return <Spinner />;
-
-    const items = this.renderItems(itemList);
-
-    return <ul className="item-list list-group">{items}</ul>;
-  }
+    return (
+        <ul className="item-list list-group">
+            { items }
+        </ul>
+    )
 }
+
+const { getAllPeople } = new SwapiService()
+
+
+
+export default withData(ItemList, getAllPeople)
